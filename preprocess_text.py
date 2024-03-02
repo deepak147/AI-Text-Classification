cb17ed10b8fec3ae841a3cc6f3ba13a9d5d9a97c
Deepak Kumar <36923302+deepak147@users.noreply.github.com>
2024-03-01 21:01:24 -0600

    Update preprocess_text.py

--- preprocess_text.py
+++ preprocess_text.py
@@ -1,8 +1,17 @@
+from google.colab import drive
+import pandas as pd
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import KFold
+from sklearn.metrics import accuracy_score
 from nltk.tokenize import word_tokenize
+import shap
+
+df = pd.read_ecxel('Two classes - ChatGPT Study Data.xlsx', engine = 'openpyxl')
 
 def preprocess_text(text):
     stopwords = [
-        "I",
         "a",
         "about",
         "an",
@@ -40,3 +49,10 @@ def preprocess_text(text):
     words = word_tokenize(text.lower())
     text = ' '.join([word for word in words if word.isalpha() and word not in stopwords])
     return text
+
+df['text'] = df['text, cleaned'].apply(preprocess_text)
+
+
+# Split data into labels and text
+labels = df['author_type']
+texts = df['text']
